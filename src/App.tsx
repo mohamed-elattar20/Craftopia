95821a98a443dc785e334afcfa9b9340e983d2ca
Mahmoud Elbassiony <mahmoud.elbassiouny95@gmail.com>
2023-11-02 14:31:18 +0200

    changing primary & secondry colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import logo from "./assets/images/logo4-04.png";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center pt-5 fs-1">Hello World</h1>
+      <h1 className="text-center pt-5 fs-1 text-secondary">Hello World</h1>
+      <img src={logo} alt="" />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./main.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = ReactDOM.createRoot(
