a4c5cdfd650f437fa8e430aadf1f9f30fed62ea0
Mohamed Elattar <mohamedelattarm819@gmail.com>
2023-11-09 14:06:50 +0200

    Add React-firebase-hooks-library

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,24 +1,14 @@
-// Your web app's Firebase configuration
-<<<<<<< HEAD
-export const firebaseConfig = {
-    apiKey: "AIzaSyCerXBzIJ7f-LpfL3mz5GHDj_jolaZOc1o",
-    authDomain: "craftopia-405b6.firebaseapp.com",
-    projectId: "craftopia-405b6",
-    storageBucket: "craftopia-405b6.appspot.com",
-    messagingSenderId: "391694631914",
-    appId: "1:391694631914:web:554214c0e8fc370528a893"
-};
+import { initializeApp } from "@firebase/app";
 
-=======
-const firebaseConfig = {
+// Your web app's Firebase configuration
+export const firebaseConfig = {
   apiKey: "AIzaSyCerXBzIJ7f-LpfL3mz5GHDj_jolaZOc1o",
   authDomain: "craftopia-405b6.firebaseapp.com",
   projectId: "craftopia-405b6",
   storageBucket: "craftopia-405b6.appspot.com",
   messagingSenderId: "391694631914",
-  appId: "1:391694631914:web:554214c0e8fc370528a893",
+  appId: "1:391694631914:web:554214c0e8fc370528a893"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
->>>>>>> main
